988f3e0d0b8c2b72538e66ddb9060ca51cd143bc
Florian B <briand.flo13@gmail.com>
2023-06-14 14:29:05 +0200

    nmap.py fonctionnel

--- nmap.py
+++ nmap.py
@@ -1,5 +1,8 @@
 import requests
 
+TIMEOUT = 1
+
+
 def extract_ip_and_ports(report):
     lines = report.strip().split('\n')
     ip_ports = []
@@ -10,7 +13,7 @@ def extract_ip_and_ports(report):
             # split the line to get the IP
             ip = line.split()[4]
         # if the line match with x/tcp open
-        if "/tcp" in line:
+        if "/tcp" and "open" in line:
             # split the line to get the port
             port = line.split()[0].split("/")[0]
             ip_ports.append((ip, port))
@@ -19,14 +22,18 @@ def extract_ip_and_ports(report):
 
 def get_webpage(ip, port):
     url = f"http://{ip}:{port}"
+
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(f"Page found at {url}")
-        else:
-            print(f"Page not found at {url}")
+        # Send a request to the URL if no response is received within 10 seconds, an exception will be raised
+        response = requests.get(url, verify=False, timeout=TIMEOUT)
+        print(f"Found {url} with status code {response.status_code}")
+        return (ip, port)
+
+        # If an exception is raised
     except requests.exceptions.RequestException as e:
         print(f"Error connecting to {url}: {str(e)}")
+        return None
+
 
 def main():
     report = open("nmap.txt", "r").read()
@@ -34,6 +41,16 @@ def main():
     ip_ports = extract_ip_and_ports(report)
 
     print(ip_ports)
+
+    list_webpage = []
+
+    for ip, port in ip_ports:
+        print(f"Checking {ip}:{port}")
+        webpage = get_webpage(ip, port)
+        if webpage:
+            list_webpage.append(webpage)
+
+    print(list_webpage)
 
 
 if __name__ == "__main__":
